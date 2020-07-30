e6b42f4ba42a5cedbbcbef4ba045498cf9493ed3
April Guffey <65494624+AMGuffey@users.noreply.github.com>
2020-07-29 23:07:00 -0400

    linked js to html in prep for carousel, corrected errors

--- javascript.js
+++ javascript.js
@@ -1,27 +1,25 @@
-// Is this the correct setup for a Validation Form? How do I connect it to my HTML/CSS?
-
-// function ValidationForm() {
-//     let username = document.forms["RegForm"]["Name"];
-//     let email = document.forms["RegForm"]["Email"];
-//     if (username.value == "") {
-//       alert("Please enter your name.");
-//       username.focus();
-//       return false;
-//     }
-//     if (email.value == "") {
-//       alert("Please enter a valid e-mail address.");
-//       email.focus();
-//       return false;
-//     }
-//     if (email.value.indexOf("@", 0) < 0) {
-//       alert("Please enter a valid e-mail address.");
-//       email.focus();
-//       return false;
-//     }
-//     if (email.value.indexOf(".", 0) < 0) {
-//       alert("Please enter a valid e-mail address.");
-//       email.focus();
-//       return false;
-//     }
-//     return true;
-//   } 
+ function ValidationForm() {
+     let username = document.forms["RegForm"]["Name"];
+     let email = document.forms["RegForm"]["Email"];
+     if (username.value == "") {
+       alert("Please enter your name.");
+       username.focus();
+       return false;
+     }
+     if (email.value == "") {
+       alert("Please enter a valid e-mail address.");
+       email.focus();
+       return false;
+     }
+     if (email.value.indexOf("@", 0) < 0) {
+       alert("Please enter a valid e-mail address.");
+       email.focus();
+       return false;
+     }
+     if (email.value.indexOf(".", 0) < 0) {
+       alert("Please enter a valid e-mail address.");
+       email.focus();
+       return false;
+     }
+     return true;
+   } 
